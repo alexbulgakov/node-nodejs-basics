7d38fcadc0655d7f877d1fc86e72e4aa4c0f3459
alexbulgakov <contact.bulgakov@gmail.com>
2024-01-28 23:01:24 +0700

    refactor: add comments

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -5,22 +5,29 @@ import { fileURLToPath } from "node:url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const copy = async () => {
+  //* Define source and destination directories
   const sourceDir = path.join(__dirname, "files");
   const destinationDir = path.join(__dirname, "files_copy");
 
   try {
+    //* Check if source directory exists
     await fs.access(sourceDir);
     try {
+      //* Check if destination directory exists
       await fs.access(destinationDir);
+      //* Throw error if destination directory exists
       throw new Error("FS operation failed");
     } catch (error) {
+      //* If destination directory does not exist, copy files from source to destination
       if (error.code === "ENOENT") {
         await fs.cp(sourceDir, destinationDir, { recursive: true });
       } else {
+        //* If error is not ENOENT, throw the error
         throw error;
       }
     }
   } catch (error) {
+    //* If error accessing source directory, throw an error
     throw new Error("FS operation failed");
   }
 };
--- src/fs/create.js
+++ src/fs/create.js
@@ -5,11 +5,15 @@ import { fileURLToPath } from "node:url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const create = async () => {
+  //* Define the file path
   const filePath = path.join(__dirname, "files", "fresh.txt");
+
   try {
+    //* Check if the file exists
     await fs.access(filePath);
     throw new Error("FS operation failed");
   } catch (error) {
+    //* If the file does not exist, write to the file
     if (error.code === "ENOENT") {
       await fs.writeFile(filePath, "I am fresh and young");
     } else {
