73d43e3e4694f33579f5034ff855373f5daa2cc9
alexbulgakov <contact.bulgakov@gmail.com>
2024-01-29 03:05:55 +0700

    feat: implement function that reads data from `process.stdin`, reverses text using Transform Stream and then writes it into `process.stdout`

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { Transform } from "node:stream";
+
+const { Transform } = require("stream");
+
 const transform = async () => {
-    // Write your code here 
+  //* Create a new transform stream
+  const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+      //* Reverse the input and send it to the output
+      this.push(chunk.toString().split("").reverse().join(""));
+      //* Notify the stream that the transformation is complete
+      callback();
+    },
+  });
+
+  /**
+   ** Pipe the standard input to the transform stream and then pipe the
+   ** transformed output to the standard output
+   */
+  process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
