50c22c89191ef4d101fa4a16d8773c701b4e7b26
alexbulgakov <contact.bulgakov@gmail.com>
2024-01-29 02:46:04 +0700

    feat: implement function that reads file `fileToRead.txt` content using Readable Stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fileToRead.txt");
+
+  //* Create a read stream for the file
+  const readStream = fs.createReadStream(filePath, "utf8");
+
+  //* Log the file data to the console
+  readStream.on("data", (chunk) => process.stdout.write(chunk));
+
+  readStream.on("error", (error) => console.error("Error:", error.message));
 };
 
 await read();
