5791ab7f6773f868b829bfa33b35435c2a5504f8
alexbulgakov <contact.bulgakov@gmail.com>
2024-01-28 22:03:34 +0700

    feat: implement function that creates new file fresh.txt

--- Readme.md
+++ Readme.md
@@ -1,3 +1,1 @@
-# Node.js basics
-
-## !!! Please don't submit Pull Requests to this repository !!!
+## The repository contains my solutions to problems from the first unit of The Rolling Scopes NodeJS 2024 Q1 course
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import * as fs from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+  try {
+    await fs.access(filePath);
+    throw new Error("FS operation failed");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await fs.writeFile(filePath, "I am fresh and young");
+    } else {
+      throw error;
+    }
+  }
 };
 
 await create();
